b3aed6c00e7d7dd58faa19eee2da1592fa2713ab
tunnckoCore <mameto_100@mail.bg>
2015-08-25 19:48:32 +0300

    rel, logos, minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 'use strict'
 
+var fs = require('fs')
+var path = require('path')
 var gulp = require('gulp')
 var rename = require('gulp-rename')
 var cssnano = require('gulp-cssnano')
+var htmlmin = require('gulp-htmlmin')
+var inliner = require('html-inline')
 var sourcemaps = require('gulp-sourcemaps')
 var autoprefixer = require('gulp-autoprefixer')
+var concat = require('concat-stream')
 
-gulp.task('default', function() {
+gulp.task('css', function() {
   return gulp.src('src/css/*.css')
     .pipe(sourcemaps.init())
     .pipe(autoprefixer({
@@ -20,4 +25,19 @@ gulp.task('default', function() {
     .pipe(gulp.dest('dist/css/'))
 })
 
-gulp.watch('src/**/*', ['default'])
+gulp.task('html', ['inline'], function() {
+  return gulp.src('./dist/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('inline', function () {
+  return fs.createReadStream(__dirname + '/index.html')
+    .pipe(inliner())
+    .pipe(fs.createWriteStream('dist/index.html'))
+})
+
+
+gulp.task('watch', function () {
+  return gulp.watch('src/**/*', ['css'])
+})
